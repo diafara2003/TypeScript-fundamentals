60ab0b16d9116811eef4234c84d63268965e5383
Jhonnatan Urueña <jhonnatan.uruea@SINCO.NET>
2018-11-07 17:37:45 -0500

    :varaibles

---
+++ variables.js
@@ -0,0 +1,13 @@
+var number1;
+var Firstname;
+var decision;
+var _null;
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+var numbers = [0, 1, 2];
+var Names = ["Andres", "Jhonnatan"];
---
+++ variables.ts
@@ -0,0 +1,18 @@
+
+let number1: number;
+
+let Firstname: String;
+
+let decision: boolean;
+
+let _null: null | undefined;
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+let numbers: Array<number> = [0, 1, 2];
+let Names: Array<String> = ["Andres", "Jhonnatan"];
