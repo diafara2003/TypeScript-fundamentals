4ecd46136860064251432b191c3d95c5be6e93ee
Jhonnatan Urueña <jhonnatan.uruea@SINCO.NET>
2018-12-17 12:45:51 -0500

    :hola

--- clases.ts
+++ clases.ts
@@ -1,21 +1,20 @@
 class Operation {
+  private num1: number;
+  private num2: number;
 
-    private num1: number;
-    private num2: number;
+  constructor(n1: number, n2: number) {
+    this.num1 = n1;
+    this.num2 = n2;
+  }
 
-    constructor(n1: number, n2: number) {
-        this.num1 = n1;
-        this.num2 = n2;
+  getSuma(): Number {
+    return this.num1 + this.num2;
+  }
 
-    }
-
-    getSuma(): Number {
-        return this.num1 + this.num2;
-    }
-
-    // SumaArrow = (): number => { return this.num2 + this.num1; }
-
+  SumaArrow = (): number => {
+    return this.num2 + this.num1;
+  };
 }
 
 let sum = new Operation(3, 4).getSuma();
-let sumArrow=new Operation(3,4).SumaArrow();
+let sumArrow = new Operation(3, 4).SumaArrow();
